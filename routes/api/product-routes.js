2dfd668df0fc2915c7cc5fc8cc6e651036c49bfd
apersonius <personius12@gmail.com>
2023-05-06 12:22:01 -0500

    delete comments

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   Product.findAll(
     {
       include: [
@@ -31,7 +28,6 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   Product.findOne(
     {
       where: {
