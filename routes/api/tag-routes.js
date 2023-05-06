b8d33e14d14af337cea88c8525dad1df10498025
apersonius <personius12@gmail.com>
2023-05-06 12:38:38 -0500

    add delete tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -81,6 +81,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: 'no tag with taht ID'});
+      return;
+    }
+    res.json(tagData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
